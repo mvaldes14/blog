327721e81e95dc7260b4442c4bfaa55930fe423a
mvaldes <mvaldes.revilla@gmail.com>
2023-07-20 15:04:21 -0500

    fixing rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,16 +1,15 @@
 import rss from "@astrojs/rss";
-import { blog } from "../lib/markdoc/frontmatter.schema";
+import { post } from "../lib/markdoc/frontmatter.schema";
 import { readAll } from "../lib/markdoc/read";
 import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from "../config";
 
 export const get = async () => {
   const posts = await readAll({
     directory: "blog",
-    frontmatterSchema: blog,
+    frontmatterSchema: post,
   });
 
   const sortedPosts = posts
-    .filter((p) => p.frontmatter.draft !== true)
     .sort(
       (a, b) =>
         new Date(b.frontmatter.date).valueOf() -
@@ -23,18 +22,6 @@ export const get = async () => {
   baseUrl = baseUrl.replace(/\/+$/g, "");
 
   const rssItems = sortedPosts.map(({ frontmatter, slug }) => {
-    if (frontmatter.external) {
-      const title = frontmatter.title;
-      const pubDate = frontmatter.date;
-      const link = frontmatter.url;
-
-      return {
-        title,
-        pubDate,
-        link,
-      };
-    }
-
     const title = frontmatter.title;
     const pubDate = frontmatter.date;
     const description = frontmatter.description;
