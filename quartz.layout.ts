bdeaec903dad098e3a556328f8b9b4af8382110b
mvaldes <mvaldes.revilla@gmail.com>
2025-09-02 13:24:06 -0500

    reader mode only desktop

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
           grow: true,
         },
         { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
+        { Component: Component.DesktopOnly(Component.ReaderMode()) },
       ],
     }),
     Component.MobileOnly(Component.Spacer()),
